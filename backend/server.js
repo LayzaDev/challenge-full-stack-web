311db888bde708c7cb901d91817ea36f947b7d32
LayzaDev <layzanauanedev@gmail.com>
2024-08-24 20:43:16 -0300

    feat: Tratamento de erros em server.js

--- backend/server.js
+++ backend/server.js
@@ -4,68 +4,141 @@ import cors from "cors";
 
 const prisma = new PrismaClient();
 
-// criando uma instância do Express
 const app = express();
 app.use(cors());
-// garante que o Express entenda o formato JSON
 app.use(express.json());
 
-// rota POST para criar os estudantes
 app.post("/students", async (request, response) => {
-  await prisma.student.create({
-    data: {
+  try {
+    const student = {
       ra: request.body.ra,
       cpf: request.body.cpf,
       email: request.body.email,
       name: request.body.name,
-    },
-  });
-  response.status(201).json(request.body);
+    };
+
+    if (!student.ra || !student.cpf || !student.email || !student.name) {
+      return response.status(400).json("Todos os campos são obrigatórios.");
+    }
+
+    const existingStudent = await prisma.student.findFirst({
+      where: {
+        OR: [
+          { ra: student.ra },
+          { cpf: student.cpf },
+          { email: student.email },
+        ],
+      },
+    });
+
+    if (existingStudent) {
+      const field =
+        existingStudent.ra === student.ra
+          ? "RA"
+          : existingStudent.cpf === student.cpf
+          ? "CPF"
+          : "E-mail";
+
+      return response
+        .status(400)
+        .json(`Este ${field} já está cadastrado no sistema.`);
+    }
+    const studentCreated = await prisma.student.create({
+      data: student,
+    });
+    return response.status(201).json(studentCreated);
+  } catch (error) {
+    return response.status(500).json(error);
+  }
 });
 
-// rota GET para listar todos os estudantes
 app.get("/students", async (request, response) => {
-  const students = await prisma.student.findMany();
-  response.status(200).json(students); // Sucesso, tudo ok!
+  try {
+    const students = await prisma.student.findMany();
+    return response.status(200).json(students);
+  } catch (error) {
+    return response.status(500).json(error);
+  }
 });
 
-// rota GET para listar um estudante
 app.get("/students/:ra", async (request, response) => {
-  const ra = request.params.ra;
-  const students = await prisma.student.findUnique({
-    where: {
-      ra: ra,
-    },
-  });
-  response.status(200).json(students); // Sucesso, tudo ok!
+  try {
+    const ra = request.params.ra;
+    const students = await prisma.student.findUnique({
+      where: {
+        ra: ra,
+      },
+    });
+    return response.status(200).json(students);
+  } catch (error) {
+    return response.status(500).json(error);
+  }
 });
 
-// rota PUT para atualizar os dados dos estudantes
 app.put("/students/:ra", async (request, response) => {
-  await prisma.student.update({
-    where: {
-      ra: request.params.ra,
-    },
-    data: {
-      cpf: request.body.cpf,
+  try {
+    const data = {
       email: request.body.email,
       name: request.body.name,
-    },
-  });
-  response.status(201).json(request.body);
+    };
+
+    if (!data.email || !data.name) {
+      return response.status(400).json("Todos os campos são obrigatórios.");
+    }
+
+    const studentSavedInDataBase = await prisma.student.findFirst({
+      where: {
+        ra: request.params.ra,
+      },
+    });
+
+    if (!studentSavedInDataBase) {
+      return response.status(404).json("Estudante não encontrado no sistema.");
+    }
+
+    const previousEmail = studentSavedInDataBase.email;
+
+    const newEmail = data.email;
+
+    const emailChanged = previousEmail != newEmail;
+
+    console.log("Chegou aqui");
+    if (emailChanged) {
+      const alreadyExistsStudentWithEmail = await prisma.student.findFirst({
+        where: {
+          email: newEmail,
+        },
+      });
+
+      if (alreadyExistsStudentWithEmail) {
+        return response
+          .status(400)
+          .json("Já existe um estudante com esse e-mail.");
+      }
+    }
+    const studentUpdated = await prisma.student.update({
+      where: {
+        ra: request.params.ra,
+      },
+      data: data,
+    });
+    return response.status(201).json(studentUpdated);
+  } catch (error) {
+    return response.status(500).json(error);
+  }
 });
 
-// rota DELETE para deletar estudantes
 app.delete("/students/:ra", async (request, response) => {
   await prisma.student.delete({
     where: {
       ra: request.params.ra,
     },
   });
-  response.status(201).json({ message: "Estudante deletado com sucesso!" });
+  return response
+    .status(201)
+    .json({ message: "Estudante deletado com sucesso!" });
 });
 
-// iniciando o servidor na porta 3000
 app.listen(3000, () => {
   console.log("Servidor rodando na porta 3000.");
 });
